702cc10d9d8e747831e599b8e3f086db040fa537
Webtourismus.at <entwicklung@webtourismus.at>
2023-09-12 18:23:40 +0200

    Bumped metapackage, many new frontend templates and configs

--- web/themes/custom/frontend/js/frontend.js
+++ web/themes/custom/frontend/js/frontend.js
@@ -4,6 +4,7 @@
 
   document.addEventListener('alpine:init', () => {
     Alpine.store('scrolled', false); /* @see <data id="scroll_observer"> */
+    Alpine.store('linkify', true); /* @see Drupal.behaviors.linkify */
     setCurrentBreakpoint(); /* @see <data id="scroll_observer"> */
   });
 
@@ -12,15 +13,21 @@
       once('linkify', '[data-js-behavior-linkify]', context).forEach((element) => {
         let action = element.getAttribute('data-js-behavior-linkify');
         let link = element.parentNode.querySelector(`a[data-action-link-type=${action}]`);
+        if (!link) {
+          console.error('Linkify without target action');
+          element.classList.remove('cursor-pointer');
+          return;
+        }
         element.addEventListener('click', (event) => {
           if (
+            /* don't trigger on splide control elements */
             event.target.getAttribute('class')?.includes('splide') ||
-            event.target?.ownerSVGElement?.getAttribute('class')?.includes('splide')
+            /* don't trigger on SVG nodes of splide control elements */
+            event.target?.ownerSVGElement?.getAttribute('class')?.includes('splide') ||
+            /* other scripts that might want to interfere with linkify */
+            Alpine.store('linkify') == false
           ) {
             return;
-          }
-          if (event.target instanceof SVGElement) {
-            console.info(event.target);
           }
           let clickEvent = new MouseEvent('click', {bubbles: true, cancelable: true});
           link.dispatchEvent(clickEvent);
